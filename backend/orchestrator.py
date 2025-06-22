fd212df49a605bf5504d9f3538c80d0490277c46
Henry Boes <98630645+Hezy4@users.noreply.github.com>
2025-06-21 21:06:03 -0700

    Update orchestrator.py

--- backend/orchestrator.py
+++ backend/orchestrator.py
@@ -35,13 +35,14 @@ user_request = input()
 agentOne = talk(AGENT1_ID, user_request)
 agentOneProcessed = ("AGENT ONE HERE: Heres the data you need to provide the user with the help they need. do not reply to me, reply to them. Store Info (JSON Format): \n(START OF FILE)\n" + storeInfo + "\n(END OF FILE)\nthe ingredients needed (JSON Format): " + agentOne + ". NOW: return the recommendation. ")
 
-agentTwo = talk(AGENT2_ID, agentOneProcessed)
+output = talk(AGENT2_ID, agentOneProcessed)
+
 #print reply from agent 2
-print(agentTwo)
+print(output)
 
 #leave the chat open for follow up questions 
 while True:
     userReply = input("\nAny questions?\n\n")
-    agentTwo = talk(AGENT2_ID, userReply)
-    print(agentTwo)
+    output = talk(AGENT2_ID, userReply)
+    print(output)
  
