70484a0512c8d5f3a60052902ddcdcbe26a4c129
Henry Boes <98630645+Hezy4@users.noreply.github.com>
2025-06-21 21:11:49 -0700

    Update quick_test.py

--- backend/quick_test.py
+++ backend/quick_test.py
@@ -3,9 +3,9 @@ from orchestrator import talk, AGENT1_ID, AGENT2_ID
 from orchestrator import storeInfo
 
 # Send data to agents
-user_request = input("\nwhat ingredients do you want? (press enter to continue)\n")
+userReply = input("\nwhat ingredients do you want? (press enter to continue)\n")
 
-agentOne = talk(AGENT1_ID, user_request)
+agentOne = talk(AGENT1_ID, userReply)
 agentOneProcessed = ("AGENT ONE HERE: Heres the data you need to provide the user with the help they need. do not reply to me, reply to them. Store Info (JSON Format): \n(START OF FILE)\n" + storeInfo + "\n(END OF FILE)\nthe ingredients needed (JSON Format): " + agentOne + ". NOW: return the recommendation. ")
 output = talk(AGENT2_ID, agentOneProcessed)
 #print reply from agent 2
